aa1bb09abc70988683d1ad334b859d6de8428aee
JCBalabuch <balabuchj@gmail.com>
2023-07-24 10:09:31 -0400

    remove non-functional buttons in right header

--- Sections/RightHeader/RightHeader.js
+++ Sections/RightHeader/RightHeader.js
@@ -3,11 +3,6 @@ import "./RightHeader.css";
 const rightHeader = () => {
   return `
   <div class = "rightHeader">
-    <button id = "signIn">Sign in</button>
-    <button id = "logIn">Log in</button>
-    <button>
-        <img src="CestaCompraTTV.png" alt="Shopping Cart">
-    </button>
     <button id = "themeMode">
         <img id="img-mode" src="day.png" alt="Clear mode">
     </button>
